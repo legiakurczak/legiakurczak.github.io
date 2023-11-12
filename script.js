0f65e29167782097bb1950fbaf514e1107f40294
Kry5t14n <85633942+kry5t14n@users.noreply.github.com>
2023-11-12 23:57:56 +0100

    ok

--- script.js
+++ script.js
@@ -2,11 +2,6 @@ $(document).ready(function(){
     $(".legia-png").click(function(){
         $("body").addClass("colorAnimation")
         $(".legia-png").hide()
-        $(".legia").append(`
-        <div class="kurczak">
-            <img class="legiakurczak" src="res/kurczepieczone.gif" alt="">
-            <img class="legianapis" src="res/legia.gif" alt="">
-        </div>
-        `)
+        $(".kurczak").css("display", "flex")
     })
 })
